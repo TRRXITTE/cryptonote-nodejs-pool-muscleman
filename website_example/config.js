0823c5c6e86d294f2f8d3b81089819a9ee14a68f
traaitt | TRRXITTE inc <traaitt@icloud.com>
2023-01-18 19:12:42 +0100

    XTCASH Mining Pool

--- website_example/config.js
+++ website_example/config.js
@@ -1,17 +1,17 @@
-var api = "https://multi-miner.smartcoinpool.net:8119";
-let parentCoin = "Arqma"
+var api = "https://mine.traaittcash.com:3109";
+let parentCoin = "traaittCASH"
 
-var email = "support@poolhost.com";
-var telegram = "https://t.me/YourPool";
+var email = "mine@trrxitte.com";
+var telegram = "https://t.me/mineTRRXITTE";
 var discord = "https://discordapp.com/invite/YourPool";
 var facebook = "https://www.facebook.com/<YourPoolFacebook>";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
-var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
+var blockchainExplorer = "https://interface.traaittcash.com/block.html?hash={id}";
+// var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
+var transactionExplorer = "https://interface.traaittcash.com/transaction.html?hash={id}";
+//var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
